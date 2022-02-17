b8b6a4a5b24d2684a3fcb21ed9185ad52818c95d
Middaugh <Jonathan.Middaugh@gmail.com>
2022-02-16 21:49:22 -0600

    remove dist

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _EssentialMUITutorial = _interopRequireDefault(require("./Components/EssentialMUITutorial"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = {
-  EssentialMUITutorial: _EssentialMUITutorial.default
-};
